f56ebb11f4f6dbac3707622a6116a08d1db61c8d
Martin-Huỳnh <phaobong123@gmail.com>
2026-06-12 22:08:36 +0700

    [Authen][remove firebase]

--- src/SRD_Master/SafeRide.Application/Features/Auth/DTOs/FirebaseLoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace SafeRide.Application.Features.Auth.DTOs;
-
-public class FirebaseLoginRequest
-{
-    public string FirebaseIdToken { get; set; } = string.Empty;
-    public string? DeviceId { get; set; }
-    public string? DeviceName { get; set; }
-}
