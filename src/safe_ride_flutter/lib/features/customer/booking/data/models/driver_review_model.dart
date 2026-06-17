10a5839c23f594ca30c57e59020b39c615db4007
HieuTran1128 <fakerhieu1128@gmail.com>
2026-06-17 10:28:48 +0700

    [CustomerFE][PromotionList]: Create UI for Customer to select promotion

---
+++ src/safe_ride_flutter/lib/features/customer/booking/data/models/driver_review_model.dart
@@ -0,0 +1,15 @@
+class DriverReviewModel {
+  final String name;
+  final String date;
+  final double rating;
+  final String comment;
+  final String initial;
+
+  const DriverReviewModel({
+    required this.name,
+    required this.date,
+    required this.rating,
+    required this.comment,
+    required this.initial,
+  });
+}
