a1e0827c63d3cd94d8c621e74e620187efe9596e
Martin-Huỳnh <phaobong123@gmail.com>
2026-06-18 10:01:44 +0700

    [Snackbar and driver location simulator]

---
+++ src/safe_ride_flutter/lib/features/driver/dashboard/presentation/widgets/driver_bottom_nav_bar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/constants/app_strings.dart';
+
+class DriverBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const DriverBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey.withOpacity(0.1),
+            width: 1,
+          ),
+        ),
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF006B70),
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.w800,
+          fontSize: 12,
+        ),
+        unselectedLabelStyle: const TextStyle(fontSize: 12),
+        onTap: onTap,
+        items: [
+          _buildNavItem(
+            Icons.home_outlined,
+            Icons.home_filled,
+            HomeStrings.home,
+            0,
+          ),
+          _buildNavItem(
+            Icons.history,
+            Icons.history,
+            HomeStrings.activity,
+            1,
+          ),
+          _buildNavItem(
+            Icons.account_balance_wallet_outlined,
+            Icons.account_balance_wallet_rounded,
+            HomeStrings.wallet,
+            2,
+          ),
+          _buildNavItem(
+            Icons.person_outline_rounded,
+            Icons.person_rounded,
+            HomeStrings.account,
+            3,
+          ),
+        ],
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(
+    IconData icon,
+    IconData activeIcon,
+    String label,
+    int index,
+  ) {
+    bool isSelected = currentIndex == index;
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 6),
+        decoration: BoxDecoration(
+          color: isSelected ? const Color(0xFF006B70) : Colors.transparent,
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: Icon(
+          isSelected ? activeIcon : icon,
+          color: isSelected ? Colors.white : Colors.grey,
+        ),
+      ),
+      label: label,
+    );
+  }
+}
